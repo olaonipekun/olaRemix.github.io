3fc34d750b68fe7f7c4508be973aa2355d5ccc95
Ololade Olaonipekun <olaonipekunololade94@gmail.com>
2024-04-15 17:41:34 +0000

    second commit

--- olaRemixContacts/app/root.tsx
+++ olaRemixContacts/app/root.tsx
@@ -1,7 +1,9 @@
 import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
+  Outlet,
   Meta,
   Scripts,
   ScrollRestoration,
@@ -45,15 +47,15 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
         </div>
-
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
